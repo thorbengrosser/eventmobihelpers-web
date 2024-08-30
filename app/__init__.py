467d8b00836adf79e7724094b0b8f0559f5775c8
thorbengrosser <github@thorben.io>
2024-08-30 17:36:51 +0200

    Small improvements and externally hosted style sheet.

--- app/__init__.py
+++ app/__init__.py
@@ -12,15 +12,16 @@ def create_app():
     app.logger.setLevel(logging.DEBUG)
     app.config['SECRET_KEY'] = 'your_secret_key'
 
+    print("Template folder:", app.template_folder)  # Debug print
+
     # Register blueprints
     app.register_blueprint(main_blueprint)
     app.register_blueprint(delete_sessions_group, url_prefix='/delete_sessions_group')
     app.register_blueprint(add_people_to_group, url_prefix='/add_people_to_group')
     app.register_blueprint(manage_chat, url_prefix='/manage_chat')
 
-
-
     return app
+
 
 if __name__ == "__main__":
     app = create_app()
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,11 @@
-from flask import render_template
+from flask import render_template, send_from_directory
 from . import main
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+# Custom route to serve styles.css manually
+@main.route('/static/css/<path:filename>')
+def custom_static(filename):
+    return send_from_directory('static/css', filename)
